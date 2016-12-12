2442410cd401226a268ebd807e62378414c3b5bd
Alex Ballera <alexballera@gmail.com>
2016-12-12 12:07:17 -0400

    Configurando formulario

--- enviando.php
+++ enviando.php
@@ -1,19 +1,19 @@
 <?php
 if($_POST["name"] && $_POST["email"] != ""){
-	$de = $_POST["nombre"];
-	$destino = "tuemail";
+	$de = $_POST["name"];
+	$destino = "alexballera@gmail.com";
 	$asunto = "FORMULARIO DE EJEMPLO";
 	$mensaje .= "FORMULARIO."."\n";
 	$mensaje .= "\n";
-	$mensaje .= "NOMBRE: " . utf8_decode($_POST["nombre"]) ."\n";
+	$mensaje .= "name: " . utf8_decode($_POST["name"]) ."\n";
 	$mensaje .= "\n";
 	$mensaje .= "EMAIL: " . utf8_decode($_POST["email"]) ."\n";
-    $emailheader = "From: LO QUE TU QUIERAS <tuemail>\r\n";
+    $emailheader = "From: Gracias por escribirnos\r\n";
 mail($destino, $asunto, $mensaje, $emailheader) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
 echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
 	} else {
-    if($_POST["nombre"] == ""){
-    echo utf8_encode ('Por favor, indica tu nombre.');
+    if($_POST["name"] == ""){
+    echo utf8_encode ('Por favor, indica tu name.');
     exit; }
     if($_POST["email"] == ""){
     echo utf8_encode ('Por favor, indica un email de contacto.');
