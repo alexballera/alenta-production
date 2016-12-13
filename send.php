c0b35925fce81f8cd339262661ad69e1382275d5
Alex Ballera <alexballera@gmail.com>
2016-12-13 12:34:51 -0400

    Form

--- send.php
+++ send.php
@@ -1,46 +1,12 @@
 <?php
-
-
 if($_POST){
-  $nombre = strip_tags($_POST['form_name']);
-  $fecha = time();
-  $fechaFormateada = date("j/n/Y", $fecha);
-  
-  $name = $_POST["form_name"];
-  $lastname = $_POST["form_lastname"];
-  $email = $_POST["form_email"];
-  $telephone: $_POST["form_phone"];
-  $message = $_POST["form_msg"];
-  $destino = "alexballera@gmail.com";
-  $asunto = "Tienes un mensaje de $nombre ";
-  // $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
-  // $mensaje .= "\n";
-  // $mensaje .= "Nombre: $nombre \n";
-  // $mensaje .= "\n";
-  // $mensaje .= "Correo: $email \n";
-  // $mensaje .= "\n";
-  // $mensaje .= "Teléfono: $telephone \n";
-  // $mensaje .= "\n";
-  // $mensaje .= "Mensaje: $message \n";
-  // $mensaje .= "\n";
-  // $mensaje .= "Fecha: " . $fechaFormateada ."\n";
-  $header = "From: alexballera@gmail.com\r\n";
-  $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-  $header .= "Mime-Version: 1.0 \r\n";
-  $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
-    // $emailheader = "From: alexballera@gmail.com\r\n";
-mail($destino, $asunto, $email, $header);
-// echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
-// echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
-  // } else {
-//
-    // if($_POST["name"] == ""){
-    // echo utf8_encode ('Por favor, indica tu nombre.');
-    // echo '<script language="javascript">alert("Por favor, indica tu nombre.");</script>';
-    // exit; }
-    // if($_POST["email"] == ""){
-    // echo utf8_encode ('Por favor, indica un email de contacto.');
-    // echo '<script language="javascript">alert("Por favor, indica un email de contacto.");</script>';
-    // exit; }
+    $name = $_POST['form_name'];
+    $email = $_POST['form_email'];
+    $message = $_POST['form_msg'];
+    $lastname: $_POST['form_lastname'];
+    $telephone: $_POST['form__phone'];
+
+//send email
+    mail("j.andrew.sears@gmail.com", "51 Deep comment from" .$email, $message);
 }
 ?>
