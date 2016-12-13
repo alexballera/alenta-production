beb18587e4eac0fa095e77de581ed7f9fa295403
Alex Ballera <alexballera@gmail.com>
2016-12-13 13:33:42 -0400

    Form

--- send.php
+++ send.php
@@ -1,10 +1,10 @@
 <?php
 if($_POST){
     $name = $_POST['form_name'];
+    $lastname = $_POST['form_lastname'];
+    $telephone = $_POST['form_phone'];
     $email = $_POST['form_email'];
     $message = $_POST['form_msg'];
-    $lastname: $_POST['form_lastname'];
-    $telephone: $_POST['form__phone'];
 
 //send email
     mail("j.andrew.sears@gmail.com", "51 Deep comment from" .$email, $message);
