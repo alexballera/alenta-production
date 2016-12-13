49f19c7d332cbda65d4b1f5e49ea247df40f6bd5
Alex Ballera <alexballera@gmail.com>
2016-12-13 12:21:55 -0400

    Optimizando form

--- send.php
+++ send.php
@@ -2,19 +2,20 @@
 
 
 if($_POST){
-  $nombre = strip_tags($_POST['name']);
+  $nombre = strip_tags($_POST['form_name']);
   $fecha = time();
   $fechaFormateada = date("j/n/Y", $fecha);
-  $de = $_POST["name"];
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $telephone: $_POST["telephone"];
-  $message = $_POST["message"];
+  $de = $_POST["form_name"];
+  $name = $_POST["form_name"];
+  $lastname = $_POST["form_lastname"];
+  $email = $_POST["form_email"];
+  $telephone: $_POST["form_phone"];
+  $message = $_POST["form_msg"];
   $destino = "alexballera@gmail.com";
   $asunto = "Tienes un mensaje de $nombre ";
   $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
   $mensaje .= "\n";
-  $mensaje .= "Nombre: $nombre \n";
+  // $mensaje .= "Nombre: $nombre \n";
   // $mensaje .= "\n";
   // $mensaje .= "Correo: $email \n";
   // $mensaje .= "\n";
@@ -28,7 +29,7 @@ if($_POST){
   $header .= "Mime-Version: 1.0 \r\n";
   $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
     // $emailheader = "From: alexballera@gmail.com\r\n";
-mail($destino, $asunto, $mensaje, $header) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
+mail($destino, $asunto, $mensaje, $header);
 // echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
 // echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
   // } else {
