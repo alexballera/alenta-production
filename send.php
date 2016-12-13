c4d5dc98fabbda3556ebd68c9dea2820a51d2c11
Alex Ballera <alexballera@gmail.com>
2016-12-13 12:29:27 -0400

    Form

--- send.php
+++ send.php
@@ -5,7 +5,7 @@ if($_POST){
   $nombre = strip_tags($_POST['form_name']);
   $fecha = time();
   $fechaFormateada = date("j/n/Y", $fecha);
-  $de = $_POST["form_name"];
+  
   $name = $_POST["form_name"];
   $lastname = $_POST["form_lastname"];
   $email = $_POST["form_email"];
@@ -13,8 +13,8 @@ if($_POST){
   $message = $_POST["form_msg"];
   $destino = "alexballera@gmail.com";
   $asunto = "Tienes un mensaje de $nombre ";
-  $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
-  $mensaje .= "\n";
+  // $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
+  // $mensaje .= "\n";
   // $mensaje .= "Nombre: $nombre \n";
   // $mensaje .= "\n";
   // $mensaje .= "Correo: $email \n";
@@ -22,14 +22,14 @@ if($_POST){
   // $mensaje .= "Teléfono: $telephone \n";
   // $mensaje .= "\n";
   // $mensaje .= "Mensaje: $message \n";
-  $mensaje .= "\n";
-  $mensaje .= "Fecha: " . $fechaFormateada ."\n";
+  // $mensaje .= "\n";
+  // $mensaje .= "Fecha: " . $fechaFormateada ."\n";
   $header = "From: alexballera@gmail.com\r\n";
   $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
   $header .= "Mime-Version: 1.0 \r\n";
   $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
     // $emailheader = "From: alexballera@gmail.com\r\n";
-mail($destino, $asunto, $mensaje, $header);
+mail($destino, $asunto, $email, $header);
 // echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
 // echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
   // } else {
