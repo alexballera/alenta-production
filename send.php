3f720487e4419d34fd33c9fc6d9830a8113c6f8b
Alex Ballera <alexballera@gmail.com>
2016-12-13 16:13:18 -0400

    Form

--- send.php
+++ send.php
@@ -16,10 +16,11 @@ $mensaje .= "\n";
 $mensaje .= "Fecha: " . $fechaFormateada ."\n";
 $mensaje .= "\n";
 $mensaje .= "Mensaje: " . utf8_decode($_POST["form_msg"]) ."\n";
-$header = "From: alexballera@gmail.com\r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
+$headers .= 'From:' . $email. "\r\n"; // Sender's Email
+$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
 
 if($_POST["form_name"] && $_POST["form_email"] != ""){
 mail($destino, $asunto, $mensaje, $header) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
