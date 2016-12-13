80532626dc342c78b74929329ac9815581c28185
Alex Ballera <alexballera@gmail.com>
2016-12-13 16:56:52 -0400

    Form

--- send.php
+++ send.php
@@ -11,32 +11,32 @@ $fechaFormateada = date("j/n/Y", $fecha);
 $nombre = strip_tags($name);
 
 
-$destino = "alexballera@gmail.com";
+$from = "alexballera@gmail.com";
 
-$asunto = "Tienes un mensaje de $nombre";
+$subject = "Tienes un mensaje de $nombre";
 
-$mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
-$mensaje .= "\n";
-$mensaje .= "Nombre: " . $name ."\n";
-$mensaje .= "\n";
-$mensaje .= "Apellido: " . $lastname ."\n";
-$mensaje .= "\n";
-$mensaje .= "Correo: " . $email ."\n";
-$mensaje .= "\n";
-$mensaje .= "Teléfono: " . $phone ."\n";
-$mensaje .= "\n";
-$mensaje .= "Mensaje: " . $message ."\n";
-$mensaje .= "\n";
-$mensaje .= "Fecha: " . $fechaFormateada ."\n";
+$body .= "Mensaje Desde El Formulario Web Alenta."."\n";
+$body .= "\n";
+$body .= "Nombre: " . $name ."\n";
+$body .= "\n";
+$body .= "Apellido: " . $lastname ."\n";
+$body .= "\n";
+$body .= "Correo: " . $email ."\n";
+$body .= "\n";
+$body .= "Teléfono: " . $phone ."\n";
+$body .= "\n";
+$body .= "Mensaje: " . $message ."\n";
+$body .= "\n";
+$body .= "Fecha: " . $fechaFormateada ."\n";
 
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
-$headers .= 'From:' . $email. "\r\n"; // Sender's Email
-$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
+$header .= 'From:' . $email. "\r\n"; // Sender's Email
+$header .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
 
 // send email
-$success = mail($destino, $asunto, $mensaje, $header);
+$success = mail($from, $subject, $body, $header);
 
 // redirect to success page
 if ($success){
