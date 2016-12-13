deb663fa73424a5cbedbb1cc6c9a13eab21ab8ec
Alex Ballera <alexballera@gmail.com>
2016-12-13 16:23:26 -0400

    Form

--- send.php
+++ send.php
@@ -3,11 +3,14 @@
 $nombre = strip_tags($_POST['form_name']);
 $fecha = time();
 $fechaFormateada = date("j/n/Y", $fecha);
+$name = $_POST["form_name"];
+
+
 $destino = "alexballera@gmail.com";
 $asunto = "Tienes un mensaje de $nombre ";
 $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
 $mensaje .= "\n";
-$mensaje .= "Nombre: " . utf8_decode($_POST["form_name"]) ."\n";
+$mensaje .= "Nombre: " . $name ."\n";
 $mensaje .= "\n";
 $mensaje .= "Correo: " . utf8_decode($_POST["form_email"]) ."\n";
 $mensaje .= "\n";
@@ -22,6 +25,7 @@ $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
 $headers .= 'From:' . $email. "\r\n"; // Sender's Email
 $headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
 
+// if($_POST["form_name"] && $_POST["form_email"] != ""){
 if($_POST){
 mail($destino, $asunto, $mensaje, $header) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
 // echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
