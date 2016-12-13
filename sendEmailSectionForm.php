3f9f5893d0a460f1df6b7fa1eba869aa3f8a0b20
Alex Ballera <alexballera@gmail.com>
2016-12-12 20:22:05 -0400

    Configurando formulario

--- sendEmailSectionForm.php
+++ sendEmailSectionForm.php
@@ -1,9 +1,13 @@
 <?php
 
+$nombre = strip_tags($_POST['name']);
+$fecha = time();
+$fechaFormateada = date("j/n/Y", $fecha);
+
 if($_POST["name"] && $_POST["email"] != ""){
   $de = $_POST["name"];
   $destino = "alexballera@gmail.com";
-  $asunto = "Tienes un mensaje desde el sitio Web Alenta ";
+  $asunto = "Tienes un mensaje de " . $nombre .;
   $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
   $mensaje .= "\n";
   $mensaje .= "Nombre: " . utf8_decode($_POST["name"]) ."\n";
@@ -11,6 +15,8 @@ if($_POST["name"] && $_POST["email"] != ""){
   $mensaje .= "Correo: " . utf8_decode($_POST["email"]) ."\n";
   $mensaje .= "\n";
   $mensaje .= "Teléfono: " . utf8_decode($_POST["telephone"]) ."\n";
+  $mensaje .= "\n";
+  $mensaje .= "Fecha: " . $fechaFormateada ."\n";
   $mensaje .= "\n";
   $mensaje .= "Mensaje: " . utf8_decode($_POST["message"]) ."\n";
   $header = "From: alexballera@gmail.com\r\n";
