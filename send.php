59db841ba3058531178b9a1594ba7baebfdf7dc5
Alex Ballera <alexballera@gmail.com>
2016-12-13 16:09:02 -0400

    Form

--- send.php
+++ send.php
@@ -1,29 +1,27 @@
 <?php
 
+$nombre = strip_tags($_POST['form_name']);
+$fecha = time();
+$fechaFormateada = date("j/n/Y", $fecha);
+$destino = "alexballera@gmail.com";
+$asunto = "Tienes un mensaje de $nombre ";
+$mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
+$mensaje .= "\n";
+$mensaje .= "Nombre: " . utf8_decode($_POST["form_name"]) ."\n";
+$mensaje .= "\n";
+$mensaje .= "Correo: " . utf8_decode($_POST["form_email"]) ."\n";
+$mensaje .= "\n";
+$mensaje .= "Teléfono: " . utf8_decode($_POST["form_phone"]) ."\n";
+$mensaje .= "\n";
+$mensaje .= "Fecha: " . $fechaFormateada ."\n";
+$mensaje .= "\n";
+$mensaje .= "Mensaje: " . utf8_decode($_POST["form_msg"]) ."\n";
+$header = "From: alexballera@gmail.com\r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
 
 if($_POST["form_name"] && $_POST["form_email"] != ""){
-  $nombre = strip_tags($_POST['form_name']);
-  $fecha = time();
-  $fechaFormateada = date("j/n/Y", $fecha);
-  $de = $_POST["form_name"];
-  $destino = "alexballera@gmail.com";
-  $asunto = "Tienes un mensaje de $nombre ";
-  $mensaje .= "Mensaje Desde El Formulario Web Alenta."."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Nombre: " . utf8_decode($_POST["form_name"]) ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Correo: " . utf8_decode($_POST["form_email"]) ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Teléfono: " . utf8_decode($_POST["form_phone"]) ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Fecha: " . $fechaFormateada ."\n";
-  $mensaje .= "\n";
-  $mensaje .= "Mensaje: " . utf8_decode($_POST["form_msg"]) ."\n";
-  $header = "From: alexballera@gmail.com\r\n";
-  $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-  $header .= "Mime-Version: 1.0 \r\n";
-  $header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
-    // $emailheader = "From: alexballera@gmail.com\r\n";
 mail($destino, $asunto, $mensaje, $header) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
 // echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
 echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
