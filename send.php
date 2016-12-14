f85c48f2e13b3909c825f8ad57689572ab563b73
Alex Ballera <alexballera@gmail.com>
2016-12-14 08:21:43 -0400

    Insertando codigo de GetSite

--- send.php
+++ send.php
@@ -1,11 +1,11 @@
 <?php
 
-$name = $_POST["name"];
-$lastname = $_POST["lastname"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$message = $_POST["message"];
-$nombre = strip_tags($_POST['name']);
+$name = $_POST["form_name"];
+$lastname = $_POST["form_lastname"];
+$email = $_POST["form_email"];
+$phone = $_POST["form_phone"];
+$message = $_POST["form_msg"];
+$nombre = strip_tags($_POST['form_name']);
 
 $fecha = time();
 $fechaFormateada = date("j/n/Y", $fecha);
@@ -13,7 +13,7 @@ $fechaFormateada = date("j/n/Y", $fecha);
 $emailTo = "alexballera@gmail.com";
 $subject = "Nuevo mensaje de $nombre";
 
-$body .= "Mensaje Desde El Formulario Web Alenta.\n";
+$body .= "Mensaje Desde El Formulario Web Alenta."."\n";
 $body .= "\n";
 $body .= "Nombre: " . $name ."\n";
 $body .= "\n";
@@ -29,7 +29,7 @@ $body .= "Fecha: " . $fechaFormateada ."\n";
 
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain; charset=iso-8859-1 \r\n";
+$header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
 $header .= 'From:' . $email. "\r\n"; // Sender's Email
 $header .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
 
