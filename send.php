9ec7af0a177274c6730f38e82fa5f16f8c0f6905
Alex Ballera <alexballera@gmail.com>
2016-12-13 20:37:30 -0400

    Form

--- send.php
+++ send.php
@@ -1,45 +1,22 @@
 <?php
-
-$name = $_POST["form_name"];
-$lastname = $_POST["form_lastname"];
-$email = $_POST["form_email"];
-$phone = $_POST["form_phone"];
-$message = $_POST["form_msg"];
-
-$fecha = time();
-$fechaFormateada = date("j/n/Y", $fecha);
-
-$emailTo = "alexballera@gmail.com";
-$subject = "Nuevo mensaje de " . $name .;
-
-$body .= "Mensaje Desde El Formulario Web Alenta."."\n";
-$body .= "\n";
-$body .= "Nombre: " . $name ."\n";
-$body .= "\n";
-$body .= "Apellido: " . $lastname ."\n";
-$body .= "\n";
-$body .= "Correo: " . $email ."\n";
-$body .= "\n";
-$body .= "Teléfono: " . $phone ."\n";
-$body .= "\n";
-$body .= "Mensaje: " . $message ."\n";
-$body .= "\n";
-$body .= "Fecha: " . $fechaFormateada ."\n";
-
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
-$header .= 'From:' . $email. "\r\n"; // Sender's Email
-$header .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
-
-// send email
-$success = mail($emailTo, $subject, $body, $header);
-
-// redirect to success page
-if ($success){
-  echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
-}else{
-  echo '<script language="javascript">alert("Revisa los datos ingresados");</script>';
+if($_POST["form_name"] && $_POST["form_email"] != ""){
+	$de = $_POST["form_name"];
+	$destino = "alexballera@gmail.com";
+	$asunto = "FORMULARIO DE EJEMPLO";
+	$mensaje .= "FORMULARIO."."\n";
+	$mensaje .= "\n";
+	$mensaje .= "NOMBRE: " . utf8_decode($_POST["form_name"]) ."\n";
+	$mensaje .= "\n";
+	$mensaje .= "EMAIL: " . utf8_decode($_POST["form_email"]) ."\n";
+    $emailheader = "From: alexballera@gmail.com\r\n";
+mail($destino, $asunto, $mensaje, $emailheader) or die ("Lo sentimos, tu solicitud no ha sido enviada.<br/>Intentelo de nuevo.");
+echo utf8_decode(utf8_encode('Tu consulta ha sido enviada correctamente.'));
+	} else {
+    if($_POST["form_name"] == ""){
+    echo utf8_encode ('Por favor, indica tu nombre.');
+    exit; }
+    if($_POST["form_email"] == ""){
+    echo utf8_encode ('Por favor, indica un email de contacto.');
+    exit; }
 }
-
 ?>
