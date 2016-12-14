c53514048356562970c4ffb9ae4ddae4360c4ada
Alex Ballera <alexballera@gmail.com>
2016-12-14 08:06:12 -0400

    Eliminado send2

--- send.php
+++ send.php
@@ -13,7 +13,7 @@ $fechaFormateada = date("j/n/Y", $fecha);
 $emailTo = "alexballera@gmail.com";
 $subject = "Nuevo mensaje de $nombre";
 
-$body .= "Mensaje Desde El Formulario Web Alenta."."\n";
+$body .= "Mensaje Desde El Formulario Web Alenta.\n";
 $body .= "\n";
 $body .= "Nombre: " . $name ."\n";
 $body .= "\n";
@@ -29,7 +29,7 @@ $body .= "Fecha: " . $fechaFormateada ."\n";
 
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain; charset=iso-8859-1" . " \r\n";
+$header .= "Content-Type: text/plain; charset=iso-8859-1 \r\n";
 $header .= 'From:' . $email. "\r\n"; // Sender's Email
 $header .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
 
