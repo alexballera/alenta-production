cd892bcbdd663bcc094bd1dc241c12fd6ac18f58
Alex Ballera <alexballera@gmail.com>
2016-12-18 11:46:28 -0400

    Agregado un solo archivo mail php

--- sendMail.php
+++ sendMail.php
@@ -14,7 +14,8 @@ $nombre = strip_tags($name);
 $fecha = time();
 $fechaFormateada = date("j/n/Y", $fecha);
 
-$emailTo = "omayraospina@acciconsultores.com";
+$emailTo = "alexballera@gmail.com";
+// $emailTo = "omayraospina@acciconsultores.com";
 $subject = "Nuevo mensaje de $nombre";
 
 $body .= "Mensaje Desde El Formulario Web Alenta.\n";
@@ -35,7 +36,8 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain; charset=iso-8859-1 \r\n";
 $header .= 'From:' . $email. '\r\n'; // Sender's Email
-$header .= 'Cc:' . 'direccioncomercial@acciconsultores.com' . '\r\n'; // Carbon copy to Sender
+// $header .= 'Cc:' . 'direccioncomercial@acciconsultores.com' . '\r\n'; // Carbon copy to Sender
+
 
 // validate the variables ======================================================
     // if any of these variables don't exist, add an error to our $errors array
@@ -63,22 +65,25 @@ $header .= 'Cc:' . 'direccioncomercial@acciconsultores.com' . '\r\n'; // Carbon 
         // THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
 
         // show a message of success and provide a true success variable
-        $data['success'] = true;
-        mail($emailTo, $subject, $body, $header);
+        $correo = mail($emailTo, $subject, $body, $header);
 
-        // Base de Datos
-        // mysqli_connect("localhost", "usuario", "password", "base de datos");
-        // $enlace = mysqli_connect("localhost", "aballera_alex", "Juan03:16", "aballera_formularios");
-        // if (mysqli_connect_errno()) {
-        //     echo "Error: No se pudo conectar a MySQL. \r\n" . "\r\n";
-        //     echo "errno de depuración: " . mysqli_connect_errno() . "\r\n";
-        //     echo "error de depuración: " . mysqli_connect_error() . "\r\n";
-        //     exit;
-        // } else {
-        //     // mysqli_query($enlace, "INSERT INTO tabla (campo1, campo2, ... campon) VALUES ('val1, val2, ... valn)");
-        //   mysqli_query($enlace, "INSERT INTO form_element (name, lastname, phone, email, message) VALUES ('$name', '$lastname', '$phone', '$email','$msg')"); //Insert Query
-        //   mysqli_close($enlace);
-        // }
+        if ($correo) {
+          $data['success'] = true;
+          $enlace = mysqli_connect("localhost", "aballera_alex", "Juan03:16", "aballera_formularios");
+          if (mysqli_connect_errno()) {
+              echo "Error: No se pudo conectar a MySQL. \r\n" . "\r\n";
+              echo "errno de depuración: " . mysqli_connect_errno() . "\r\n";
+              echo "error de depuración: " . mysqli_connect_error() . "\r\n";
+              exit;
+          } else {
+              // mysqli_query($enlace, "INSERT INTO tabla (campo1, campo2, ... campon) VALUES ('val1, val2, ... valn)");
+            mysqli_query($enlace, "INSERT INTO form_element (name, lastname, phone, email, message) VALUES ('$name', '$lastname', '$phone', '$email','$msg')"); //Insert Query
+            mysqli_close($enlace);
+          }
+        } else {
+          $data['errors']  = 'Correo no enviado';
+        }
+
     }
 
     // return all our data to an AJAX call
