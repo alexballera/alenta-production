06cb5304b2195dda078597b3c360d33726447ca2
Alex Ballera <alexballera@gmail.com>
2016-12-17 21:02:37 -0400

    Terminado formulario slide

--- formSlide.php
+++ formSlide.php
@@ -14,7 +14,7 @@ $nombre = strip_tags($name);
 $fecha = time();
 $fechaFormateada = date("j/n/Y", $fecha);
 
-$emailTo = "alexballera@gmail.com";
+$emailTo = "omayraospina@acciconsultores.com";
 $subject = "Nuevo mensaje de $nombre";
 
 $body .= "Mensaje Desde El Formulario Web Alenta.\n";
@@ -35,6 +35,7 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain; charset=iso-8859-1 \r\n";
 $header .= 'From:' . $email. '\r\n'; // Sender's Email
+$header .= 'Cc:' . 'direccioncomercial@acciconsultores.com' . '\r\n'; // Carbon copy to Sender
 
 // validate the variables ======================================================
     // if any of these variables don't exist, add an error to our $errors array
@@ -67,17 +68,17 @@ $header .= 'From:' . $email. '\r\n'; // Sender's Email
 
         // Base de Datos
         // mysqli_connect("localhost", "usuario", "password", "base de datos");
-        $enlace = mysqli_connect("localhost", "aballera_alex", "Juan03:16", "aballera_formularios");
-        if (mysqli_connect_errno()) {
-            echo "Error: No se pudo conectar a MySQL. \r\n" . "\r\n";
-            echo "errno de depuración: " . mysqli_connect_errno() . "\r\n";
-            echo "error de depuración: " . mysqli_connect_error() . "\r\n";
-            exit;
-        } else {
-            // mysqli_query($enlace, "INSERT INTO tabla (campo1, campo2, ... campon) VALUES ('val1, val2, ... valn)");
-          mysqli_query($enlace, "INSERT INTO form_element (name, lastname, phone, email, message) VALUES ('$name', '$lastname', '$phone', '$email','$msg')"); //Insert Query
-          mysqli_close($enlace);
-        }
+        // $enlace = mysqli_connect("localhost", "aballera_alex", "Juan03:16", "aballera_formularios");
+        // if (mysqli_connect_errno()) {
+        //     echo "Error: No se pudo conectar a MySQL. \r\n" . "\r\n";
+        //     echo "errno de depuración: " . mysqli_connect_errno() . "\r\n";
+        //     echo "error de depuración: " . mysqli_connect_error() . "\r\n";
+        //     exit;
+        // } else {
+        //     // mysqli_query($enlace, "INSERT INTO tabla (campo1, campo2, ... campon) VALUES ('val1, val2, ... valn)");
+        //   mysqli_query($enlace, "INSERT INTO form_element (name, lastname, phone, email, message) VALUES ('$name', '$lastname', '$phone', '$email','$msg')"); //Insert Query
+        //   mysqli_close($enlace);
+        // }
     }
 
     // return all our data to an AJAX call
